eb99e9a353a1b7c2c2f0cd9af5efccbfd7559770
Anders Gustafsson <anders@cureos.com>
2013-12-11 21:13:49 +0100

    Preparations for PCL libraries

---
+++ Sources/Portability/_Net/Forwarders.cs
@@ -0,0 +1,12 @@
+﻿//
+// Portability Class Library
+//
+// Copyright © Cureos AB, 2013
+// info at cureos dot com
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(SerializableAttribute))]
+[assembly: TypeForwardedTo(typeof(ICloneable))]
