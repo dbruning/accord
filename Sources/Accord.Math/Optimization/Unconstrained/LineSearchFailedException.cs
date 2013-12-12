b1c6804a0d711c272a80201ca18bd753057b84db
Anders Gustafsson <anders@cureos.com>
2013-12-12 08:06:30 +0100

    Initialized portable Math project

--- Sources/Accord.Math/Optimization/Unconstrained/LineSearchFailedException.cs
+++ Sources/Accord.Math/Optimization/Unconstrained/LineSearchFailedException.cs
@@ -23,8 +23,6 @@
 namespace Accord.Math.Optimization
 {
     using System;
-    using System.Runtime.Serialization;
-    using System.Security.Permissions;
 
 
     /// <summary>
@@ -37,7 +35,7 @@ namespace Accord.Math.Optimization
     /// </remarks>
     /// 
     [Serializable]
-    public class LineSearchFailedException : Exception
+    public partial class LineSearchFailedException : Exception
     {
         int info;
 
@@ -90,45 +88,6 @@ namespace Accord.Math.Optimization
         public LineSearchFailedException(string message, Exception innerException)
             : base(message, innerException)
         { }
-
-        /// <summary>
-        ///   Initializes a new instance of the <see cref="LineSearchFailedException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// The <paramref name="info"/> parameter is null.
-        /// </exception>
-        /// <exception cref="T:System.Runtime.Serialization.SerializationException">
-        /// The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0).
-        /// </exception>
-        /// 
-        protected LineSearchFailedException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
-
-        /// <summary>
-        ///   When overridden in a derived class, sets the <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with information about the exception.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// The <paramref name="info"/> parameter is a null reference (Nothing in Visual Basic).
-        /// </exception>
-        /// <PermissionSet>
-        /// 	<IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Read="*AllFiles*" PathDiscovery="*AllFiles*"/>
-        /// 	<IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="SerializationFormatter"/>
-        /// </PermissionSet>
-        /// 
-        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            if (info == null)
-                throw new ArgumentNullException("info");
-
-            base.GetObjectData(info, context);
-
-            info.AddValue("Information", this.info);
-        }
     }
 
 }
--- Sources/Portability/_Net/Forwarders.cs
+++ Sources/Portability/_Net/Forwarders.cs
@@ -8,14 +8,11 @@
 using System;
 using System.Data;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // System
-[assembly: TypeForwardedTo(typeof(SerializableAttribute))]
 [assembly: TypeForwardedTo(typeof(ICloneable))]
+[assembly: TypeForwardedTo(typeof(NonSerializedAttribute))]
+[assembly: TypeForwardedTo(typeof(SerializableAttribute))]
 
 // System.Data
 [assembly: TypeForwardedTo(typeof(DataColumnCollection))]
-
-// System.Runtime.InteropServices
-[assembly: TypeForwardedTo(typeof(ComVisibleAttribute))]
