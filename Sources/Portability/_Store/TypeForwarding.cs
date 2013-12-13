947c581dec547c173b80bfc791462b93b79a5ee2
Anders Gustafsson <anders@cureos.com>
2013-12-13 17:04:04 +0100

    Added Store specific portability library

---
+++ Sources/Portability/_Store/TypeForwarding.cs
@@ -0,0 +1,50 @@
+﻿//
+// Portability Class Library
+//
+// Copyright © Cureos AB, 2013
+// info at cureos dot com
+//
+
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+// System
+/*[assembly: TypeForwardedTo(typeof(Converter<,>))]
+[assembly: TypeForwardedTo(typeof(DBNull))]
+[assembly: TypeForwardedTo(typeof(ICloneable))]
+[assembly: TypeForwardedTo(typeof(NonSerializedAttribute))]
+[assembly: TypeForwardedTo(typeof(NotFiniteNumberException))]
+[assembly: TypeForwardedTo(typeof(SerializableAttribute))]*/
+
+// System.Collections.Concurrent
+[assembly: TypeForwardedTo(typeof(ConcurrentStack<>))]
+
+// System.ComponentModel
+/*[assembly: TypeForwardedTo(typeof(BrowsableAttribute))]
+[assembly: TypeForwardedTo(typeof(DisplayNameAttribute))]
+
+// System.Data
+[assembly: TypeForwardedTo(typeof(DataColumn))]
+[assembly: TypeForwardedTo(typeof(DataColumnCollection))]
+[assembly: TypeForwardedTo(typeof(DataRow))]
+[assembly: TypeForwardedTo(typeof(DataRowCollection))]
+[assembly: TypeForwardedTo(typeof(DataTable))]
+[assembly: TypeForwardedTo(typeof(DataView))]
+
+// System.Diagnostics
+[assembly: TypeForwardedTo(typeof(Trace))]
+
+// System.IO
+[assembly: TypeForwardedTo(typeof(FileMode))]
+[assembly: TypeForwardedTo(typeof(FileStream))]
+
+// System.Runtime.Serialization
+[assembly: TypeForwardedTo(typeof(OptionalFieldAttribute))]
+
+// System.Runtime.Serialization.Formatters.Binary
+[assembly: TypeForwardedTo(typeof(BinaryFormatter))]*/
+
+// System.Threading.Tasks
+[assembly: TypeForwardedTo(typeof(Parallel))]
+[assembly: TypeForwardedTo(typeof(ParallelOptions))]
