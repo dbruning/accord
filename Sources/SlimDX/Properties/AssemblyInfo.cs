31e9362c9edaf11913658b10b4deb913726625e3
Anders Gustafsson <anders@cureos.com>
2014-01-16 08:25:50 +0100

    Modifications to ensure that tests can be executed using a correctly signed C++/CLI SlimDX DLL

--- Sources/SlimDX/Properties/AssemblyInfo.cs
+++ Sources/SlimDX/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Portable.SlimDX")]
+[assembly: AssemblyTitle("SlimDX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Portable.SlimDX")]
+[assembly: AssemblyCompany("SlimDX Group")]
+[assembly: AssemblyProduct("SlimDX")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.13.43")]
+[assembly: AssemblyFileVersion("4.0.13.43")]
