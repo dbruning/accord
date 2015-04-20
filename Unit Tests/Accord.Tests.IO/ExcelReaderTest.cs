e0a0e6769a279a66bc31c306d51885fc98a4ec15
Anders Gustafsson <anders@cureos.com>
2015-04-20 09:40:42 +0200

    Corrected file paths

--- Unit Tests/Accord.Tests.IO/ExcelReaderTest.cs
+++ Unit Tests/Accord.Tests.IO/ExcelReaderTest.cs
@@ -33,7 +33,7 @@ namespace Accord.Tests.IO
         [TestMethod()]
         public void ExcelReaderConstructorTest()
         {
-            string path = @"..\..\..\..\Accord.Tests.Statistics\Resources\sample.xls";
+            string path = @"..\..\..\..\Unit Tests\Accord.Tests.Statistics\Resources\sample.xls";
 
             // Create a new reader, opening a given path
             ExcelReader reader = new ExcelReader(path);
@@ -73,7 +73,7 @@ namespace Accord.Tests.IO
         [TestMethod()]
         public void ConstructorExcel8Test()
         {
-            string path = @"..\..\..\..\Accord.Tests.Statistics\Resources\sample.xls";
+            string path = @"..\..\..\..\Unit Tests\Accord.Tests.Statistics\Resources\sample.xls";
             ExcelReader target = new ExcelReader(path);
 
             testWorksheets(target);
